0bee9bac8857860eae57860af427e7cf8f4f59cc
bright okike <brightokike@gmail.com>
2019-06-06 17:20:44 +0300

    first method

--- enumerables.rb
+++ enumerables.rb
@@ -1,5 +1,23 @@
-  module Enumerable
+module Enumerable
+    # def my_each
+    #   self.length.times do |x|
+    #     yield(self[x])
+    #   end
+    # end
     def my_each
-      # your code here
+        self.length.times do |index|
+            item = self[index]
+            yield(item)
+        end
     end
-  end
+end
+
+arr = [2,3,4,5,6,8,9,10,7]
+
+def test1(arr)
+    include Enumerable
+    arr.my_each do |i|
+      "#{i * 2} "
+    end
+end
+print test1(arr)
