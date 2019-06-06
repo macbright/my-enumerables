ce7b976d0492db4300399f1c487cc060762912a9
bright okike <brightokike@gmail.com>
2019-06-06 17:50:22 +0300

    third method added

--- enumerables.rb
+++ enumerables.rb
@@ -12,14 +12,26 @@ module Enumerable
         index += 1
       end
     end
+
+    def my_select
+      arr = Array.new
+      self.length.times do |x|
+        if yield(self[x])
+          arr.push(x)
+        end
+      end
+      arr
+    end
     
 end
 
 def test1(arr)
     include Enumerable
-    arr.my_each_with_index do |i|
-      print i
+    arr.my_select do |i|
+      if i % 2 == 0
+        print i
+      end
     end
 end
-arr = [2,3,4,5,6,8,9,10,7]
+arr = [2,3,4,5,6,8,9,10,12]
 test1(arr) 
