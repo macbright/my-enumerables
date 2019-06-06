47350b49d98780059c3176ce1412da97e0bf9164
bright okike <brightokike@gmail.com>
2019-06-06 17:34:27 +0300

    second method added

--- enumerables.rb
+++ enumerables.rb
@@ -1,23 +1,25 @@
 module Enumerable
-    # def my_each
-    #   self.length.times do |x|
-    #     yield(self[x])
-    #   end
-    # end
     def my_each
-        self.length.times do |index|
-            item = self[index]
-            yield(item)
-        end
+      self.length.times do |x|
+        yield(self[x])
+      end
     end
-end
 
-arr = [2,3,4,5,6,8,9,10,7]
+    def my_each_with_index
+      index = 0
+      self.length.times do |x|
+        yield(index)
+        index += 1
+      end
+    end
+    
+end
 
 def test1(arr)
     include Enumerable
-    arr.my_each do |i|
-      "#{i * 2} "
+    arr.my_each_with_index do |i|
+      print i
     end
 end
-print test1(arr)
+arr = [2,3,4,5,6,8,9,10,7]
+test1(arr) 
